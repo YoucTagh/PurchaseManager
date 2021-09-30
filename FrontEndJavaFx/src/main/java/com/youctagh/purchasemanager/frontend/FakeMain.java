182f8314525b3bd3f59526e1c78407ab26e8c1b5
YoucTagh <youc.learn.13@gmail.com>
2021-09-30 22:25:08 +0200

    fix executable with fake main

---
+++ FrontEndJavaFx/src/main/java/com/youctagh/purchasemanager/frontend/FakeMain.java
@@ -0,0 +1,12 @@
+package com.youctagh.purchasemanager.frontend;
+
+import com.youctagh.purchasemanager.frontend.application.Launcher;
+
+/**
+ * @author YoucTagh
+ */
+public class FakeMain {
+    public static void main(String[] args) {
+        Launcher.main(args);
+    }
+}
